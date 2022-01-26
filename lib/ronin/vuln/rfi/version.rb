fcf9f723a22c1c47ad865e8aeee55868bef7743a
Postmodern <postmodern.mod3@gmail.com>
2022-01-26 14:29:40 -0800

    Added a `ronin/vuln/rfi/version` file.

---
+++ lib/ronin/vuln/rfi/version.rb
@@ -0,0 +1,28 @@
+#
+# ronin-vuln-rfi - A small Ruby library for testing Remote File Inclusion (RFI)
+# vulnerabilities
+#
+# Copyright (c) 2007-2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# ronin-vuln-rfi is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-vuln-rfi is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-vuln-rfi.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+module Ronin
+  module Vuln
+    class RFI
+      # ronin-vuln-rfi version
+      VERSION = '0.1.0'
+    end
+  end
+end
