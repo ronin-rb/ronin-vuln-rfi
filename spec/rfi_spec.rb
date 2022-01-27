a9822b310b4673111c3d25f9eb70f600f0659cbe
Postmodern <postmodern.mod3@gmail.com>
2022-01-26 23:59:27 -0800

    Refactored specs for RSpec 3.

--- spec/rfi_spec.rb
+++ spec/rfi_spec.rb
@@ -1,26 +1,39 @@
 require 'spec_helper'
 require 'ronin/vuln/rfi'
 
-describe Vuln::RFI do
-  it "should have a valid TEST_SCRIPT URL" do
-    response = Net.http_get(:url => Vuln::RFI::TEST_SCRIPT)
+describe Ronin::Vuln::RFI do
+  describe "TEST_SCRIPT" do
+    subject { described_class::TEST_SCRIPT }
 
-    response.code.to_i.should == 200
-    response.body.should_not be_empty
+    it "must be a publically accessible URL", :network do
+      response = Net::HTTP.get(URI(subject))
+
+      expect(response.code.to_i).to eq(200)
+      expect(response.body).to_not be_empty
+    end
   end
 
-  it "should have a default test_script URL" do
-    Vuln::RFI.test_script.should == PHP::RFI::TEST_SCRIPT
+  describe ".test_script" do
+    subject { described_class }
+
+    it "must have a default test_script URL" do
+      expect(subject.test_script).to eq(described_class::TEST_SCRIPT)
+    end
   end
 
-  it "should allow configuration of the test_script URL" do
-    new_url = 'http://www.example.com/test.php'
+  describe ".test_script=" do
+    subject { described_class }
 
-    Vuln::RFI.test_script = new_url
-    Vuln::RFI.test_script.should == new_url
-  end
+    let(:new_url) { 'http://www.example.com/test.php' }
 
-  after(:all) do
-    Vuln::RFI.test_script = nil
+    before do
+      subject.test_script = new_url
+    end
+
+    it "must set .test_script URL" do
+      expect(subject.test_script).to eq(new_url)
+    end
+
+    after { subject.test_script = nil }
   end
 end
